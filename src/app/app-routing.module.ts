de702906492d6b3d61c346aa2b81ee2aa5c0989f
Jose Montero <lbo2mail@gmail.com>
2020-10-09 13:06:49 -0300

    Rutas basicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { SeleccionComponent } from './components/seleccion/seleccion.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'seleccion', component: SeleccionComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: HomeComponent },  // Wildcard route for a 404 page
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
